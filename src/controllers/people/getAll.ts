d0fe09ab57c8eaa95812fb63c88ce9ef9568c102
Amsterdan Vasconcelos <amsterdan.contato@gmail.com>
2025-01-01 18:19:45 -0300

    feat: create getAll people controller

---
+++ src/controllers/people/getAll.ts
@@ -0,0 +1,43 @@
+import { RequestHandler } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+import { peopleServices } from '@/services';
+import { getJsonError } from '@/utils';
+import { validation } from '@/middlewares';
+import { querySchema, type QueryProps } from '@/schemas/people/getAll';
+
+const getAllValidator = validation({ query: querySchema });
+
+type GetAll = RequestHandler<unknown, unknown, unknown, QueryProps>;
+
+const getAll: GetAll = async (req, res) => {
+  const { page, limit, search } = req.query;
+
+  const result = await peopleServices.getAll({
+    page: Number(page),
+    limit: Number(limit),
+    search,
+  });
+
+  const count = await peopleServices.count(search);
+
+  if (result instanceof Error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(getJsonError(result.message));
+    return;
+  }
+  if (count instanceof Error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(getJsonError(count.message));
+    return;
+  }
+
+  res.setHeader('access-control-expose-headers', 'x-total-count');
+  res.setHeader('x-total-count', count);
+
+  res.status(StatusCodes.OK).json(result);
+};
+
+export { getAll, getAllValidator };
--- src/controllers/people/index.ts
+++ src/controllers/people/index.ts
@@ -1,9 +1,11 @@
 import * as create from './create';
 import * as deleteById from './deleteById';
+import * as getAll from './getAll';
 
 const peopleController = {
   ...create,
   ...deleteById,
+  ...getAll,
 };
 
 export { peopleController };
