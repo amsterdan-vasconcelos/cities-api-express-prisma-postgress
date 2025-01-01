9fe9262abb27fb875be79d6ec03c93e3eb5499ea
Amsterdan Vasconcelos <amsterdan.contato@gmail.com>
2025-01-01 18:30:43 -0300

    feat: added route handling in Express

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import helmet from 'helmet';
-import { home } from './routes';
+import { home, users, cities, people } from './routes';
 
 const app = express();
 
@@ -8,5 +8,8 @@ app.use(express.json());
 app.use(helmet());
 
 app.use(home.router);
+app.use(users.router);
+app.use(cities.router);
+app.use(people.router);
 
 export default app;
