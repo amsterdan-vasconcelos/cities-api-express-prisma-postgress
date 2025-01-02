24de6901ad585664ced94c514ae5732818483263
Amsterdan Vasconcelos <amsterdan.contato@gmail.com>
2025-01-02 16:49:22 -0300

    test: create create cities test

---
+++ tests/cities/create.test.ts
@@ -0,0 +1,38 @@
+import { StatusCodes } from 'http-status-codes';
+import { testServer, accessToken } from '../jest.setup';
+
+describe('Citys - CREATE', () => {
+  it('Cria registro.', async () => {
+    const response = await testServer
+      .post('/cities')
+      .set({ authorization: `Bearer ${accessToken}` })
+      .send({ name: 'fortaleza' });
+
+    expect(response.statusCode).toEqual(StatusCodes.CREATED);
+    expect(typeof response.body).toEqual('number');
+  });
+  it('Não cria registro sem mandar a propriedade name.', async () => {
+    const response = await testServer
+      .post('/cities')
+      .set({ authorization: `Bearer ${accessToken}` })
+      .send({ nam: 'fortaleza' });
+
+    expect(response.statusCode).toEqual(StatusCodes.BAD_REQUEST);
+    expect(response.body).toHaveProperty('errors.body.name');
+  });
+  it('Não cria registro se o name contém um número de caracteres abaixo do 2.', async () => {
+    const response = await testServer
+      .post('/cities')
+      .set({ authorization: `Bearer ${accessToken}` })
+      .send({ name: 'c' });
+
+    expect(response.statusCode).toEqual(StatusCodes.BAD_REQUEST);
+    expect(response.body).toHaveProperty('errors.body.name');
+  });
+  it('Tenta criar um registro sem token de acesso.', async () => {
+    const response = await testServer.post('/cities').send({ name: 'carmem' });
+
+    expect(response.statusCode).toEqual(StatusCodes.UNAUTHORIZED);
+    expect(response.body).toHaveProperty('errors.default');
+  });
+});
--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -1,5 +1,35 @@
 import supertest from 'supertest';
+import { StatusCodes } from 'http-status-codes';
 
 import app from '@/app';
 
-export const testServer = supertest(app);
+let accessToken = '';
+
+const generateToken = async () => {
+  const resRegister = await testServer.post('/signup').send({
+    name: 'Amsterdan',
+    email: 'super-unico-email@email.com',
+    password: '12345678',
+  });
+
+  expect(resRegister.statusCode).toEqual(StatusCodes.CREATED);
+  expect(typeof resRegister.body).toEqual('number');
+
+  const resLogin = await testServer.post('/signin').send({
+    email: 'super-unico-email@email.com',
+    password: '12345678',
+  });
+
+  expect(resLogin.statusCode).toEqual(StatusCodes.OK);
+  expect(resLogin.body).toHaveProperty('accessToken');
+
+  accessToken = resLogin.body.accessToken;
+};
+
+beforeAll(async () => {
+  await generateToken();
+});
+
+const testServer = supertest(app);
+
+export { testServer, accessToken };
