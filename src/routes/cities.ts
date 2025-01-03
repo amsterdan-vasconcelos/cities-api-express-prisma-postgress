220f0e682516f1015f6cb9362ee89c0931e93e7c
Amsterdan Vasconcelos <amsterdan.contato@gmail.com>
2025-01-03 10:28:27 -0300

    refactor: update ensureAuthenticated function call

--- src/routes/cities.ts
+++ src/routes/cities.ts
@@ -5,37 +5,34 @@ import { ensureAuthenticated } from '@/middlewares';
 
 const router = Router();
 
+router.use(ensureAuthenticated);
+
 router.get(
   '/cities',
-  ensureAuthenticated,
   citiesController.getAllValidator,
   citiesController.getAll,
 );
 
 router.get(
   '/cities/:id',
-  ensureAuthenticated,
   citiesController.getByIdValidator,
   citiesController.getById,
 );
 
 router.post(
   '/cities',
-  ensureAuthenticated,
   citiesController.createValidator,
   citiesController.create,
 );
 
 router.put(
   '/cities/:id',
-  ensureAuthenticated,
   citiesController.updateByIdValidator,
   citiesController.updateById,
 );
 
 router.delete(
   '/cities/:id',
-  ensureAuthenticated,
   citiesController.deleteByIdValidator,
   citiesController.deleteById,
 );
--- src/routes/people.ts
+++ src/routes/people.ts
@@ -5,37 +5,34 @@ import { ensureAuthenticated } from '@/middlewares';
 
 const router = Router();
 
+router.use(ensureAuthenticated);
+
 router.get(
   '/people',
-  ensureAuthenticated,
   peopleController.getAllValidator,
   peopleController.getAll,
 );
 
 router.get(
   '/people/:id',
-  ensureAuthenticated,
   peopleController.getByIdValidator,
   peopleController.getById,
 );
 
 router.post(
   '/people',
-  ensureAuthenticated,
   peopleController.createValidator,
   peopleController.create,
 );
 
 router.put(
   '/people/:id',
-  ensureAuthenticated,
   peopleController.updateByIdValidator,
   peopleController.updateById,
 );
 
 router.delete(
   '/people/:id',
-  ensureAuthenticated,
   peopleController.deleteByIdValidator,
   peopleController.deleteById,
 );
